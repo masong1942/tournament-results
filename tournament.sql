0e8dac3845622de42118f419803eb633643fb974
Gary Mason <masong@sbcglobal.net>
2015-03-31 13:55:16 -0700

    Committed by Udacity

---
+++ tournament.sql
@@ -0,0 +1,22 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+create table players (
+    id  serial       primary key,
+  name  varchar(50)  not null
+);
+
+
+create table matches (
+  id      serial   primary key,
+  winner  integer  not null  references players(id),
+  loser   integer  not null  references players(id)
+);
+
+
